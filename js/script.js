9ca93159fd8c3b3be4face020a99b3e92f7c5613
TairSadyk <tairsadykov92@gmail.com>
2022-06-18 21:40:29 -0400

    flip card functionality is added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 
 const displayFile = document.querySelector('#display-file');
 const input = document.querySelector('.form__input');
+const formCont = document.querySelector('.form-container');
+const cardArt = document.querySelector('.cards-article');
+const cardTextFront = document.querySelector('.card__text--front');
+const cardTextBack = document.querySelector('.card__text--back');
 let dataArr;
 input.addEventListener('change', function () {
   const filename = this.value.split('\\').at(-1);
@@ -11,8 +15,12 @@ input.addEventListener('change', function () {
   Papa.parse(file, {
     dynamicTyping: true,
     complete: function (results) {
-      console.log('Finished:', results.data);
       dataArr = results.data;
+      formCont.classList.add('hidden');
+      cardArt.classList.remove('hidden');
+      const randNum = Math.floor(Math.random() * 20);
+      cardTextFront.innerText = dataArr[randNum][0];
+      cardTextBack.innerText = dataArr[randNum][1];
     },
   });
 });
